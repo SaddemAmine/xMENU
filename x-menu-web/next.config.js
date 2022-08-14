0f2be00b0a034daa15a34e03d78e267912b28885
SaddemAmine <naceuramine.saddem@esprit.tn>
2022-08-14 13:14:09 +0100

    init

---
+++ x-menu-web/next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
+
+const withAntdLess = require('next-plugin-antd-less');
+
+module.exports = withAntdLess({
+  // lessVarsFilePath: './styles/variables.less',
+  // lessVarsFilePathAppendToEndOfContent: false,
+  // cssLoaderOptions: {},
+
+  webpack(config) {
+    return config;
+  },
+});
